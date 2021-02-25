4855d9757420a1242dae9f7fbe29291dabe7fbc7
Arkhe002 <79669404+Arkhe002@users.noreply.github.com>
2021-02-26 00:52:41 +0300

    Update CSGO.java

--- plugin/src/main/java/me/badbones69/crazycrates/cratetypes/CSGO.java
+++ plugin/src/main/java/me/badbones69/crazycrates/cratetypes/CSGO.java
@@ -11,7 +11,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
